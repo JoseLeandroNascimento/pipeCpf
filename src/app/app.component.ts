6de0b014f302260948ef87486527b6aa351e5c77
unknown <j.leandro1998silva@gmail.com>
2024-05-31 18:10:50 -0500

    criação do pipe de formatação de cpf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CpfPipe } from './shared/pipes/cpf.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CpfPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pipe-cpf';
+
+  public cpf: string = "89965441081";
+
 }
